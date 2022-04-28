51d7044d246cc8d6c48e25f5e6c9dbc51bb7c1fb
Jonathan Rustrian <jonathanmiusic34@gmail.com>
2022-04-28 14:35:57 -0500

    creacion de usuario y registro

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::get('/dat', function () {
     return DB::select('select * from TRABAJADOR');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

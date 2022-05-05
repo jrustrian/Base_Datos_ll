059978d606b6a6a0d24badd902f50c6c02f563f0
Jonathan Rustrian <jonathanmiusic34@gmail.com>
2022-05-04 22:46:00 -0500

    controlador futuro arreglado

--- app/Http/Controllers/FuturoController.php
+++ app/Http/Controllers/FuturoController.php
@@ -17,7 +17,6 @@ class FuturoController extends Controller
         $futuros = DB::table('future_movements')
 
             ->join('movement_types','movement_types.id','=', 'future_movements.type')
-            ->select('future_movements.*','accounts.name as accounts','movement_types.name as types')
             ->select('future_movements.*','movement_types.name as types')
             ->get();
 

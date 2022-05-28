49d5414f53e56092b6243ad608b2b3a517952f78
Luis Villatoro <lvillatorom4@miumg.edu.gt>
2022-05-28 00:06:45 -0600

    Cambio de Diseños

--- resources/views/deuda/registro.blade.php
+++ resources/views/deuda/registro.blade.php
@@ -8,8 +8,14 @@
     @include('mensajes.messages')
 
     <div class="form-group">
+<<<<<<< Updated upstream
         <input type="text" class="form-control" name="users_id" id="users_id"
                value="1" hidden>
+=======
+        <label for="users_id">Usuario</label>
+        <input type="text" class="form-control {{$errors->has('users_id')? 'is-invalid':''}}" name="users_id" id="users_id"
+               value="{{isset($deuda->users_id)?$deuda->users_id:old('users_id') }}">
+>>>>>>> Stashed changes
     </div>
 
         <div class="form-group">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{ route('home') }}">FINANZAS</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Sistema de Finanzas</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -55,7 +55,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        CUENTAS
+                        Cuentas
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('cuenta-agregar') }}">Agregar</a>
